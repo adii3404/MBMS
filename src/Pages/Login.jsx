478322a15a1ea57814a3f371524d13ce74d76c43
pradippawar4667-lang <pradippawar4667@gmail.com>
2026-02-04 09:59:07 +0530

    login page changes

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-box">
-        <h1>MediCare Login</h1>
+        <h1  style={{color: '#50c127'}}>MediCare Login</h1>
         <p style={{marginBottom: '20px', color: '#666'}}>Please sign in to continue</p>
         
         {error && <div style={{color: 'red', marginBottom: '10px'}}>{error}</div>}
